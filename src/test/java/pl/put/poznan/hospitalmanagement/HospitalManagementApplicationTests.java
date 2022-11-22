a1d99c97bfe4494f8fb61b60ebf3a61c93da43af
Krzysztof Piasecki <krzysztof.piasecki1410@gmail.com>
2022-11-22 19:00:44 +0100

    created project

---
+++ src/test/java/pl/put/poznan/hospitalmanagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.hospitalmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
